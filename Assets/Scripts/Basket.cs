580649b5ffc63be6c4ca5d49e973598d45f3c291
Ben Marriner <bmarriner@deakin.edu.au>
2022-10-17 19:45:07 +1100

    Implemented fruit collecting

---
+++ Assets/Scripts/Basket.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class Basket : MonoBehaviour
+{
+    private Interactable interactable;
+
+    [SerializeField] private bool isHeld;
+    public Collider basketCollider;
+    public Collider fruitTrigger;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        interactable = GetComponent<Interactable>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (interactable.attachedToHand)
+        {
+            isHeld = interactable.attachedToHand.ObjectIsAttached(gameObject);
+        }
+        else isHeld = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //if (!(other.gameObject.layer == LayerMask.NameToLayer("Fruit"))) return;
+
+        if (!IsHoldingBasketAndFruit(other.gameObject, out var fruitComponent)) return;
+
+        Physics.IgnoreCollision(other, basketCollider, true);
+        fruitComponent.isBeingHeldInBasket = true;
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        //if (!(other.gameObject.layer == LayerMask.NameToLayer("Fruit"))) return;
+
+        if (!IsHoldingBasketAndFruit(other.gameObject, out var fruitComponent)) return;
+
+        fruitComponent.isBeingHeldInBasket = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        //if (!(other.gameObject.layer == LayerMask.NameToLayer("Fruit"))) return;
+
+        IsHoldingBasketAndFruit(other.gameObject, out var fruitComponent);
+        if (!fruitComponent) return;
+
+        Physics.IgnoreCollision(other, basketCollider, false);
+        fruitComponent.isBeingHeldInBasket = false;
+    }
+
+    private bool IsHoldingBasketAndFruit(GameObject obj, out Fruit outFruitComponent)
+    {        
+        outFruitComponent = null;
+
+        if (!isHeld) return false; // Is the player holding the basket?
+        if (!interactable.attachedToHand) return false; // Is the player holding an object?
+        if (!interactable.attachedToHand.otherHand.ObjectIsAttached(obj)) return false; // Is it in their other hand?
+        if (!obj.TryGetComponent<Fruit>(out var fruitComponent)) return false; // Is the object a fruit?
+
+        outFruitComponent = fruitComponent;
+        return true;
+    }
+}
---
+++ Assets/Scripts/Fruit.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.WindowsRuntime;
+using UnityEditor.VersionControl;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class Fruit : MonoBehaviour
+{
+    private Interactable interactable;
+
+    [HideInInspector] public bool isBeingHeldInBasket;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        interactable = GetComponent<Interactable>();
+        interactable.onDetachedFromHand += Interactable_onDetachedFromHand;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(isBeingHeldInBasket);
+    }
+
+    // Place fruit in basket
+    private void Interactable_onDetachedFromHand(Hand hand)
+    {
+        if (isBeingHeldInBasket)
+        {
+            var basket = hand.otherHand.currentAttachedObject;
+            GetComponent<Rigidbody>().isKinematic = true;
+            hand.currentAttachedObject.transform.SetParent(basket.transform);
+        }
+        else
+        {
+            GetComponent<Rigidbody>().isKinematic = false;
+        }
+
+        Debug.Log("Detaching from hand");
+    }
+
+}
